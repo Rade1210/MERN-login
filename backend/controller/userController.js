81b0e67cbc56d4647754583de9fd665b6ba89808
Rade1210 <radepetrovic200@gmail.com>
2023-05-21 13:47:53 +0200

    Definisane rute i middleware

---
+++ backend/controller/userController.js
@@ -0,0 +1,45 @@
+
+
+import asyncHandler from "express-async-handler"
+
+// Auth user/set token
+// route POST /api/users/auth
+// Public
+
+const authUser = asyncHandler(async(req, res) => { // asyncHandler automatically does try/catch
+    res.status(200).json({message: "Auth User"})
+})
+
+// Register a new user
+// route POST /api/users
+// Public
+
+const registerUser = asyncHandler(async(req, res) => { // asyncHandler automatically does try/catch
+    res.status(200).json({message: "Register user"})
+})
+
+// Logout user
+// route POST /api/users/logout
+// Public
+
+const logoutUser = asyncHandler(async(req, res) => { // asyncHandler automatically does try/catch
+    res.status(200).json({message: "Logout user"})
+})
+
+// Get user profile
+// route GET /api/users/profile
+// Private
+
+const getUserProfile = asyncHandler(async(req, res) => { // asyncHandler automatically does try/catch
+    res.status(200).json({message: "User profile"})
+})
+
+// Update user profile
+// route PUT /api/users/profile
+// Private
+
+const updateUserProfile = asyncHandler(async(req, res) => { // asyncHandler automatically does try/catch
+    res.status(200).json({message: "Update user profile"})
+})
+
+export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile }
