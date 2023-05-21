c298292fe83eb5bba1ed9160798d2cb53c73115e
Rade1210 <radepetrovic200@gmail.com>
2023-05-21 20:12:06 +0200

    It generates http only jwt cookie

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler"
+import generateToken from "../utils/generateToken.js"
 import User from "../models/userModel.js"
 
 // Auth user/set token
@@ -14,7 +15,30 @@ const authUser = asyncHandler(async(req, res) => { // asyncHandler automatically
 // Public
 
 const registerUser = asyncHandler(async(req, res) => { // asyncHandler automatically does try/catch
-    console.log(req.body)
+    const { name, email, password } = req.body
+    const userExists = await User.findOne({email})
+    if(userExists){
+        res.status(400)
+        throw new Error("User already exists")
+    }
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        generateToken(res, user._id) // if user exists, it generates a token and sends it to client
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error("Invalid user data")
+    }
+
     res.status(200).json({message: "Register user"})
 })
 
---
+++ backend/utils/generateToken.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken"
+
+//jwt adds userId in payload
+const generateToken = (res, userId) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "1h"
+    })
+
+res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: "3600"
+})
+}
+
+export default generateToken
