418c241086b94199a9353f7abec6fd5733c0f548
Rade1210 <radepetrovic200@gmail.com>
2023-05-23 12:13:49 +0200

    Backend finished

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,91 +1,121 @@
-import asyncHandler from "express-async-handler"
-import generateToken from "../utils/generateToken.js"
-import User from "../models/userModel.js"
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
-// Auth user/set token
-// route POST /api/users/auth
-// Public
+// @desc    Auth user & get token
+// @route   POST /api/users/auth
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-const authUser = asyncHandler(async(req, res) => { // asyncHandler automatically does try/catch
+  const user = await User.findOne({ email });
 
-    const { email, password } = req.body
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
 
-    const user = await User.findOne({email})
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
 
-    if(user && (await user.matchPasswords(password))){
-        generateToken(res, user._id)
-        res.status(201).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-        })
-    } else {
-        res.status(401)
-        throw new Error("Invalid email or password")
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
+// @desc    Logout user / clear cookie
+// @route   POST /api/users/logout
+// @access  Public
+const logoutUser = (req, res) => {
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: 'Logged out successfully' });
+};
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
     }
-})
 
-// Register a new user
-// route POST /api/users
-// Public
+    const updatedUser = await user.save();
 
-const registerUser = asyncHandler(async(req, res) => { // asyncHandler automatically does try/catch
-    const { name, email, password } = req.body
-    const userExists = await User.findOne({email})
-    if(userExists){
-        res.status(400)
-        throw new Error("User already exists")
-    }
-    
-    const user = await User.create({
-        name,
-        email,
-        password
-    })
-
-    if(user){
-
-        generateToken(res, user._id) // if user exists, it generates a token and sends it to client
-        res.status(201).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email
-        })
-
-    } else {
-        res.status(400)
-        throw new Error("Invalid user data")
-    }
-
-    res.status(200).json({message: "Register user"})
-})
-
-// Logout user
-// route POST /api/users/logout
-// Public
-
-const logoutUser = asyncHandler(async(req, res) => { // asyncHandler automatically does try/catch
-    res.cookie("jwt", '', {
-        httpOnly:true,
-        expires: new Date(0),
-    })
-    res.status(200).json({message: "User logged out"})
-})
-
-// Get user profile
-// route GET /api/users/profile
-// Private
-
-const getUserProfile = asyncHandler(async(req, res) => { // asyncHandler automatically does try/catch
-    res.status(200).json({message: "User profile"})
-})
-
-// Update user profile
-// route PUT /api/users/profile
-// Private
-
-const updateUserProfile = asyncHandler(async(req, res) => { // asyncHandler automatically does try/catch
-    res.status(200).json({message: "Update user profile"})
-})
-
-export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile }
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,39 +1,42 @@
-import mongoose from "mongoose"
-import bcrypt from "bcryptjs"
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-}, {
-    timestamps: true
-})
+  },
+  {
+    timestamps: true,
+  }
+);
 
-userSchema.pre("save", async function(next) {
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-    if(!this.isModified("password")) { // if pass is not modified
-        next()
-    }
+// Encrypt password using bcrypt
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
 
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
-})
+const User = mongoose.model('User', userSchema);
 
-userSchema.methods.matchPasswords = async function(enteredPassword){
-    return await bcrypt.compare(enteredPassword, this.password)
-}
-
-const User = mongoose.model("User", userSchema)
-
-export default User
+export default User;
